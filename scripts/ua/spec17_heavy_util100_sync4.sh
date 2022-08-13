3876c33ba83ac6811d38c7c19037f37537b16557
Zhewen Pan <zhewen@Zhewens-MBP.lan>
2022-08-13 00:12:00 -0500

    update

---
+++ scripts/ua/spec17_heavy_util100_sync4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ $# -ne 1 ] 
+then 
+    echo Usage: ./spec17_heavy_util100_sync4.sh procid
+    exit
+fi
+case $1 in
+  0)
+    core=64
+    mix=heavy
+    sync=4
+    util=100
+    ;;
+  *)
+    echo "bad procid"
+    exit 1;;
+esac
+
+./run.sh $core $mix $sync $util
